229078e2488ef761db576d4de9c51a0fcb24a3be
yusufoverkill <57762613+yusufoverkill@users.noreply.github.com>
2019-11-14 22:11:29 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -189,6 +189,20 @@ function claim($token)
         return false;
         }
     }
+    function claim3($token)
+    {
+    $data = '{"promo_code":"GOFOODPRAKTIS19"}';    
+    $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
+    if ($claim['success'] == 1)
+        {
+        return $claim['data']['message'];
+        }
+      else
+        {
+      save("error_log.txt", json_encode($claim));
+        return false;
+        }
+    }
      function ride($token)
     {
     $data = '{"promo_code":"COBAINGOJEK"}';    
